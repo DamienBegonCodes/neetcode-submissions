88165e2dcf5071812b0eeea83b34d87364221902
Damien Begon <78902686+DamienBegonCodes@users.noreply.github.com>
2026-08-05 19:29:58 +0200

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) 
+    {
+        int l(0), r(nums.size() - 1), mid((l + r) / 2);
+
+        while (l <= r)
+        {
+            if (target < nums[mid])
+            {
+                r = mid - 1;
+            }
+            else if (target > nums[mid])
+            {
+                l = mid + 1;
+            }
+            else
+            {
+                return mid;
+            }
+            mid = (l + r) / 2;
+        }
+        return -1;
+    }
+};
