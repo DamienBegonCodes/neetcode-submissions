db0404399443a5edad10318653c76f915352e6ee
Damien Begon <78902686+DamienBegonCodes@users.noreply.github.com>
2026-09-17 12:45:41 +0200

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) 
+    {
+        vector<vector<int>> out({{}});
+
+        int i(0), n(nums.size());
+        while (i < n)
+        {
+            int j(0), out_size(out.size());
+            while(j < out_size)
+            {
+                vector<int> copy(out[j]);
+                copy.push_back(nums[i]);
+                out.push_back(copy);
+                ++j;
+            }
+            ++i;
+        }
+
+        return out;
+    }
+};
